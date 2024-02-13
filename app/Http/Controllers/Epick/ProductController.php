1e1c5bdd0e55ec73d12ca831c5a8b84980a6219d
jakebogan01 <jakebogan01@gmail.com>
2024-02-13 12:20:15 -0500

    creates list of products

---
+++ app/Http/Controllers/Epick/ProductController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Epick;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Inertia\Inertia;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // view pagination results
+        // return Product::latest()->paginate();
+
+        return Inertia::render('Projects/Epick/Index', [
+            'products' => Product::latest()->simplePaginate(6),
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+            'laravelVersion' => Application::VERSION,
+            'phpVersion' => PHP_VERSION,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- resources/js/Pages/Projects/Epick/Index.svelte
+++ resources/js/Pages/Projects/Epick/Index.svelte
@@ -3,22 +3,23 @@
 </script>
 
 <script>
-    import { Link, page } from "@inertiajs/svelte";
+    import { Link, page, inertia } from "@inertiajs/svelte";
     export let canLogin;
     export let canRegister;
     export let laravelVersion;
     export let phpVersion;
+    export let products;
 </script>
 
 <svelte:head>
     <title>Epick | Home</title>
 </svelte:head>
 
-<h1>Epick home</h1>
+<h1>HOME | All Products</h1>
 
 {#if canLogin}
     <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-        {#if $page.props.auth.user}
+        {#if $page?.props.auth.user}
             <Link
                 href="/dashboard"
                 class="text-sm text-gray-700 underline dark:text-white"
@@ -44,3 +45,28 @@
         {/if}
     </div>
 {/if}
+
+{#each products?.data as product (product?.id)}
+    <div class="border border-gray-200 rounded p-4 my-2">
+        <h2 class="text-xl">{product?.title}</h2>
+        <p>${@html product?.price}</p>
+        <img src={product?.image} alt="">
+    </div>
+{/each}
+
+<div>
+    <div class="mt-6">
+        {#if products?.prev_page_url === null || products?.prev_page_url === undefined}
+            <span class="px-1">&laquo; Prev</span>
+        {:else}
+            <a use:inertia href="{products?.first_page_url}" class="px-1">First Page</a>
+            <a use:inertia href="{products?.prev_page_url}" class="px-1">&laquo; Prev</a>
+        {/if}
+
+        {#if products?.next_page_url === null || products?.next_page_url === undefined}
+            <span class="px-1">Next &raquo;</span>
+        {:else}
+            <a use:inertia href="{products?.next_page_url}" class="px-1">Next &raquo;</a>
+        {/if}
+    </div>
+</div>
