7d19d3b5b4f0ecf88e2d8f211319603d6a4b7363
jakebogan01 <jakebogan01@gmail.com>
2024-02-15 15:32:03 -0500

    any product with category furniture has size of null

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -38,7 +38,10 @@ class DatabaseSeeder extends Seeder
         Project::factory()->create([
             'name' => 'Epick',
         ])->products()->createMany(
-            Product::factory()->count(30)->make()->toArray()
+            Product::factory()->count(30)->make([
+                'category_id' => Category::where('name', 'Furniture')->first()->id,
+                'size' => null,
+            ])->toArray()
         );
 
         Project::factory()->create([
