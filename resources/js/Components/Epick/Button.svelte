efea031ae692ec30340e3457a1cb8aa9a8ca5ba5
jakebogan01 <jakebogan01@gmail.com>
2024-02-22 14:15:37 -0500

    styled new on home page

--- resources/js/Components/Epick/Button.svelte
+++ resources/js/Components/Epick/Button.svelte
@@ -1,4 +1,9 @@
-<button type="button" class="flex items-center justify-center text-white text-sm font-light bg-[#1E1E21] py-4 px-6 mt-6 w-full rounded">
+<script>
+    /* svelte-ignore unused-export-let */
+    export let bkg = 'bg-[#1E1E21]';
+</script>
+
+<button type="button" class="flex items-center justify-center text-white text-sm font-light {bkg} py-4 px-6 mt-6 w-full rounded">
     <slot name="btnIcon">
         <svg xmlns="http://www.w3.org/2000/svg" width="15.493" height="21.422"><g fill="none" stroke="currentColor" stroke-miterlimit="10"><path d="M11.7 6.1h3.294v14.822H.5V6.1h11.2Z"/><path d="M4.453 8.076V3.794a3.294 3.294 0 1 1 6.588 0v4.282"/></g></svg>
     </slot>
--- resources/js/Components/Epick/GradientHeading.svelte
+++ resources/js/Components/Epick/GradientHeading.svelte
@@ -2,7 +2,7 @@
     /* svelte-ignore unused-export-let */
     export let margin = '';
     /* svelte-ignore unused-export-let */
-    export let bkg = 'bg-gradient-to-t from-black to-[#87878F]';
+    export let bkg = 'bg-gradient-to-t from-black to-[#87878F] from-10%';
 </script>
 
 <span class="{margin} {bkg} bg-clip-text" style="-webkit-text-fill-color: transparent;"><slot /></span>
