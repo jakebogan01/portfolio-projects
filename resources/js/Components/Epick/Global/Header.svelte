4b6c129141e74f7312726f70eac22f6b0e0545eb
jakebogan01 <jakebogan01@gmail.com>
2024-02-23 14:38:54 -0500

    styled brand icons on home page table

--- resources/js/Components/Epick/Global/Header.svelte
+++ resources/js/Components/Epick/Global/Header.svelte
@@ -7,15 +7,15 @@
 </script>
 
 <header class="text-white mt-[107px]">
-    <h1 class="font-montserrat text-7xl font-bold text-nowrap leading-[44px]">
+    <h1 class="font-montserrat text-7xl md:text-[105px] font-bold text-nowrap leading-[44px] md:leading-[85px]">
         <slot name="headingText">
             <GradientHeading styles="sm:ml-[30px]">We fetch</GradientHeading>
             <br>
-            <span class="text-4xl">You select</span>
+            <span class="text-4xl md:text-7xl">You select</span>
         </slot>
     </h1>
 
-    <p class="text-[#A3A6A7] mt-10">
+    <p class="text-[#A3A6A7] mt-10 max-w-[500px]">
         <slot name="introText">
             Explore endless choices at our online store – diverse categories, quality products, unbeatable prices. Your one-stop shop for convenience and satisfaction. Shop smarter with us today.
         </slot>
