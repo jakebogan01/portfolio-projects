e8fe275952ec7bab9b52d85decada8db85e5c8d8
jakebogan01 <jakebogan01@gmail.com>
2024-02-24 18:44:18 -0500

    styled cta banner on the home page desktop

--- resources/js/Pages/Projects/Epick/Home/Index.svelte
+++ resources/js/Pages/Projects/Epick/Home/Index.svelte
@@ -103,7 +103,7 @@
     </section>
 
     <aside id="ctaBanner">
-        <div class="relative bg-[#181A1B] py-12 shadow-[0px_0px_0px_100vmax_rgb(24,26,27)] sm:shadow-none sm:rounded-lg sm:px-12 md:px-[92px] sm:py-[75px]" style="clip-path: inset(0 -100vmax);">
+        <div class="relative bg-[#181A1B] py-12 shadow-[0px_0px_0px_100vmax_rgb(24,26,27)] sm:shadow-none sm:rounded-lg sm:px-12 md:px-[146px] sm:py-[86px] max-w-[968px] mx-auto" style="clip-path: inset(0 -100vmax);">
             <div class="relative text-center sm:text-left z-0">
                 <h3 class="font-roboto text-xl sm:text-2xl">Elevate your style, click and smile</h3>
                 <span class="inline-block text-[#A3A6A7] text-base pb-1">Buy Now, up to 40% off.</span>
@@ -111,7 +111,7 @@
                     <span slot="btnText" class="ml-2 sm:ml-4">Buy now</span>
                 </Button>
             </div>
-            <img src="/images/epick/tablet/home/shopping-bags.png" alt="" class="hidden sm:block absolute right-0 bottom-0 -z-10" role="presentation">
+            <img src="/images/epick/tablet/home/shopping-bags.png" alt="" class="hidden sm:block absolute right-0 top-1/2 -translate-y-1/2 w-[26%] -z-10" role="presentation">
         </div>
     </aside>
 </main>
