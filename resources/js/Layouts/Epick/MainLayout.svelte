74b19873887da1b69012af5efdc24521abba15d6
jakebogan01 <jakebogan01@gmail.com>
2024-02-28 15:00:21 -0500

    adds background hover effect

--- resources/js/Layouts/Epick/MainLayout.svelte
+++ resources/js/Layouts/Epick/MainLayout.svelte
@@ -3,11 +3,29 @@
     import Footer from "@/Components/Epick/Global/Footer/Footer.svelte";
     import CtaBanner from "@/Components/Epick/Global/CtaBanner.svelte";
     import Notification from "@/Components/Epick/Global/Notification.svelte";
+    import {onMount} from "svelte";
     /* svelte-ignore unused-export-let */
     export let canLogin, canRegister, cartQuantity, flash;
+
+    onMount(() => {
+        const handleOnMouseMove = e => {
+            const { currentTarget: target } = e;
+
+            const rect = target.getBoundingClientRect(),
+                    x = e.clientX - rect.left,
+                    y = e.clientY - rect.top;
+
+            target.style.setProperty("--mouse-x", `${x}px`);
+            target.style.setProperty("--mouse-y", `${y}px`);
+        }
+
+        for(const light of document.querySelectorAll(".light")) {
+            light.onmousemove = e => handleOnMouseMove(e);
+        }
+    });
 </script>
 
-<div class="container sm:max-w-[1440px] overflow-hidden">
+<div class="container light h-max sm:max-w-[1440px] overflow-hidden">
     <Nav {canLogin} {canRegister} {cartQuantity} />
 
     <main>
